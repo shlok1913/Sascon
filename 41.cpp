463114fa0e9174e5aae9ac4f1111e175b0b052f6
Shlok Srivastava <125036865+shlok1913@users.noreply.github.com>
2023-02-10 19:18:13 +0530

    Add files via upload

---
+++ 41.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int middleOfThree(int a, int b, int c)
+{
+    // Checking for b
+    if ((a < b && b < c) || (c < b && b < a))
+       return b;
+ 
+    // Checking for a
+    else if ((b < a && a < c) || (c < a && a < b))
+       return a;
+ 
+    else
+       return c;
+}
+int main() {
+    int query;
+    cin>>query;
+    while(query) {
+        int n;
+        cin>>n;
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<middleOfThree(a,b,c);
+        query--; 
+    }
+}
